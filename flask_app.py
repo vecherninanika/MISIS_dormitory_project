ec5bb58988708e519d3ae8ce07725826133aff93
vecherninanika <nikavecher333@gmail.com>
2025-12-23 11:42:32 +0300

    new

--- flask_app.py
+++ flask_app.py
@@ -144,4 +144,4 @@ def get_fitting_room(name):
         return jsonify({"error": error}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
